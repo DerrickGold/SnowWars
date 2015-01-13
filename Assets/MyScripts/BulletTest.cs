b79f7fbdc7965b2c96df2ccee734c8f982776fce
Derrick <derrick.b.gold@gmail.com>
2015-01-13 16:41:27 -0700

    Initial push of test stuff

---
+++ Assets/MyScripts/BulletTest.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class BulletTest : MonoBehaviour {
+
+
+	public GameObject projectile;
+	private AudioSource audio;
+
+
+	// Use this for initialization
+	void Start () {
+		//audio = GetComponentInChild<AudioSource> ();audio
+		audio = GetComponentInChildren<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1"))
+		{
+			Rigidbody instantiatedProjectile = Instantiate(projectile.rigidbody,
+			                                               transform.position,
+			                                               transform.rotation)
+				as Rigidbody;
+
+			instantiatedProjectile.AddForce (transform.forward * 1200.0f);
+			audio.Play ();
+		
+		}
+	}
+	
+}
---
+++ Assets/MyScripts/Projectile.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+	float speed = 60.0f;
+	bool collided = false;
+	private AudioSource audio;
+	// Use this for initialization
+	void Start () {
+		audio = GetComponent<AudioSource> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(collided && !audio.isPlaying) Destroy (gameObject);
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		audio.Play ();
+		collided = true;
+		//stop the snowball in its track
+		Destroy (rigidbody);
+		Destroy (collider);
+	}
+
+}
