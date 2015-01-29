4d80ac632a5f2fb6a75861f12669f49489c5ad2b
murrayc43 <murrayc43@mymacewan.ca>
2015-01-29 13:35:45 -0700

    ReOrganized Project Folders and Files

---
+++ Assets/Scripts/BulletTest.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class BulletTest : MonoBehaviour 
+{
+	public GameObject projectile;
+	[SerializeField]
+	private AudioSource audio;
+
+	void Update () 
+	{
+		if (Input.GetButtonDown("Fire1"))
+		{
+			Rigidbody instantiatedProjectile = Instantiate(projectile.rigidbody, transform.position, transform.rotation) as Rigidbody;
+			instantiatedProjectile.AddForce (transform.forward * 1200.0f);
+			audio.Play();
+		
+		}
+	}
+}
