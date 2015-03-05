abf6595eedb795561748af5acc338761afde9cd6
Derrick <derrick.b.gold@gmail.com>
2015-03-05 15:39:56 -0700

    Add AIController

---
+++ Assets/Scripts/AI Scripts/AIController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIController : MonoBehaviour {
+
+
+	public enum State { WALKING, PLAYERTRACK, ATTACKING, DEAD, ITEMTRACK };
+	public State state = State.WALKING;
+
+	//get location of current target to chase and kill
+	public Transform currentTarget;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch(state) {
+		case default:
+			break;
+
+
+		case State.WALKING:
+			break;
+
+		case State.PLAYERTRACK:
+			break;
+
+		case State.ATTACKING:
+			break;
+
+		case State.DEAD:
+			break;
+
+		case State.ITEMTRACK:
+			break;
+
+		}
+	}
+
+
+
+	void OnTriggerEnter(Collider collision) {
+
+	}
+
+	void OnTriggerExit(Collider collision) {
+
+	}
+
+}
