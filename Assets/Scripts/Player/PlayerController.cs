443b0887ec47596e5a122467eeb2bfb5b17fb1e1
Curtis <murrayc43@mymacewan.ca>
2015-03-05 15:08:54 -0700

    Add Player Controller
    LOLOLOLOLOLOLOLOL

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    //Player variables
+    private enum PlayerState
+    {
+        IDLE,
+        WALKING,
+        RUNNING,
+        JUMPING,
+    };
+    private PlayerState state;
+    private float hp = 100.0f;
+    private Vector3 moveDirection = Vector3.zero;
+
+    //Game variables
+    public GameObject snowball;
+
+    void Start()
+    {
+        state = PlayerState.IDLE;
+    }
+
+    void Update()
+    {
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        
+
+        switch (state)
+        {
+            case PlayerState.IDLE:
+                break;
+            case PlayerState.WALKING:
+                break;
+            case PlayerState.RUNNING:
+                break;
+            case PlayerState.JUMPING:
+                break;
+        }
+    }
+}
