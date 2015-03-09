fa766196d9f317b2a664a6eea42f232812f32061
Jay <wickins.jay@gmail.com>
2015-03-09 14:28:43 -0600

    Pickups
    Fixed character collider position. Added buff

---
+++ Assets/Scripts/JayScripts/BuffPickup.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BuffPickup : MonoBehaviour {
+
+	private float buffDisplay;
+	private bool buffAquired;
+	private Text[] buffText = new Text[2];
+	// Use this for initialization
+	void Start () {
+		buffAquired = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (buffAquired) {
+			if(Time.time-buffDisplay > 2.5f){
+				//print (Time.time + " " + buffDisplay);
+				buffText[0].gameObject.SetActive(false);
+				buffText[1].gameObject.SetActive(true);
+				buffAquired= false;
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.tag == "PickUp")
+		{
+			other.gameObject.SetActive(false); //deactivate the buff object
+			Rotator buffScript = other.gameObject.GetComponent<Rotator>(); //get the script
+			Text buff = buffScript.buffText; //get the buff info text
+			buffText[0] = buffScript.buffText; 
+			//buffText[0] = buff;
+			buffText[1] = buffScript.buffIcon; //store the buff icon text
+			//buffText = {buffScript.buffText, buffScript.buffIcon};
+			buff.gameObject.SetActive(true); //set the buff info text to true
+			buffDisplay = Time.time; 
+			buffAquired = true; //we picked up a buff
+		}
+
+	}
+}
---
+++ Assets/Scripts/JayScripts/Rotator.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Rotator : MonoBehaviour {
+
+	public Text buffText;
+	public Text buffIcon;
+	private Vector3 originalPos;
+	public float buffBounceSpeed;
+	public float buffBounceHeight;
+	void Start()
+	{
+		buffBounceSpeed = 2f;
+		buffBounceHeight = 3f;
+		originalPos = transform.position;
+		buffText = (Text)(transform.parent.transform.FindChild ("Canvas").FindChild("STEALTH").GetComponent<Text>());
+		buffText.gameObject.SetActive (false);
+		buffIcon = (Text)(transform.parent.transform.FindChild ("Canvas").FindChild("ICON").GetComponent<Text>());
+		buffIcon.gameObject.SetActive (false);
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		float t = Time.time;
+		transform.Rotate (new Vector3(15, 30, 45) * Time.deltaTime);
+		//transform.position += Vector3.up * Time.deltaTime * Mathf.Abs(Mathf.Sin(Time.deltaTime));
+		transform.position = new Vector3 (
+			originalPos.x,
+			originalPos.y + buffBounceHeight * Mathf.Abs (Mathf.Sin (Time.time*buffBounceSpeed)),
+			originalPos.z);
+	}
+}
