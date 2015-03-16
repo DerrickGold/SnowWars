c5c227a776164d5c3b3eeb718030b3f4b592ed3d
Derrick <derrick.b.gold@gmail.com>
2015-03-16 15:25:25 -0600

    Start Buff Class Add navmesh

--- Assets/Scripts/Common.cs
+++ Assets/Scripts/Common.cs
@@ -6,6 +6,21 @@ public class Common: MonoBehaviour {
 	static public int MaxPlayers = 32;
 	static public int AIViewRange = 20;
 	static public float MaxThrowForce = 1200.0f;
+
+	static public int BaseMaxHealth = 100;
+	static public int MaxHealthBoost = 20;
+	//how much health to take away for every throw
+	static public int AmmoSubtractAmmount = 1;
+	static public int SuperSnowSubtract = 5;
+
+	static public float BaseWalkSpeed = 4.0f;
+	static public float SpeedBoost = 1.0f;
+
+	static public int BaseSnowBallDamage = 15;
+	static public int SuperSnowBallBoost = 15;
+
+
+
 	static public GameObject player;
 
     //AI variables
