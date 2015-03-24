08e8aecc216f6aab3059a6f701aec8e3dcf4af41
Jay <wickins.jay@gmail.com>
2015-03-24 15:25:48 -0600

    Glowing lights

--- Assets/Scripts/JayScripts/PointLightGlow.cs
+++ Assets/Scripts/JayScripts/PointLightGlow.cs
@@ -10,6 +10,7 @@ public class PointLightGlow : MonoBehaviour {
 
 	private Light pointLight;
 	public float GLOW_SPEED = 3f; //how fast the light goes through a cycle
+	public float TIME_DELAY = 0f;
 
 	// Use this for initialization
 	void Start () {
@@ -18,8 +19,10 @@ public class PointLightGlow : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		//sin values range from -1 to 1 as time goes on
-		float t = Mathf.Sin (GLOW_SPEED*Time.time); 
-		pointLight.intensity = 1.5f + t; //intensity varies from 0.5 to 2.5
+		if (Time.time > TIME_DELAY) {
+			//sin values range from -1 to 1 as time goes on
+			float t = Mathf.Sin (GLOW_SPEED * Time.time); 
+			pointLight.intensity = 1.5f + t; //intensity varies from 0.5 to 2.5
+		}
 	}
 }
