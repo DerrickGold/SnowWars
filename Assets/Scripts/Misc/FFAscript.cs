5d73919446305fae586bd5e5a1837355a05ecd2e
syonkers <syonkers@live.com>
2015-04-05 21:50:34 -0600

    Start Free for all script
    started free for all script

--- Assets/Scripts/Misc/FFAscript.cs
+++ Assets/Scripts/Misc/FFAscript.cs
@@ -2,10 +2,19 @@
 using System.Collections;
 
 public class FFAscript : MonoBehaviour {
+	private Common common;
+	public GameObject AIprefab;
+	public GameObject player;
+
+	private int maxPlayerSize;
+	private int topScore;
+	private int secondScore;
+	private int playerScore;
 
 	// Use this for initialization
 	void Start () {
-	
+		common = GameObject.FindGameObjectWithTag ("Global").GetComponent<Common> ();
+
 	}
 	
 	// Update is called once per frame
