46a6c6f98ba6008132c3c702ae03fb6e0905ef17
Jay <wickins.jay@gmail.com>
2015-04-09 18:06:49 -0600

    Added spike trap!
    Run under the archway to trigger the falling spikes. Spikes currently
    take away 60hp

---
+++ Assets/SpikeFall.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpikeFall : MonoBehaviour {
+
+	public float FALL_SPEED = 1f;
+	private Vector3 originalPosition;
+	private bool fallen;
+	private Transform spikes;
+
+	// Use this for initialization
+	void Start () {
+		//originalPosition = transform.position;
+		spikes = transform.Find ("Trap");
+		originalPosition = spikes.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider col){
+		if (col.gameObject.tag.Contains ("Team")) StartCoroutine ("fall");
+	}
+
+	public IEnumerator fall(){
+		while (spikes.position.y > 65f) {
+			spikes.position = new Vector3(originalPosition.x, spikes.position.y-FALL_SPEED, originalPosition.z );
+			yield return null;
+		}
+		spikes.position = originalPosition;
+	}
+}
---
+++ Assets/TrapKill.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrapKill : MonoBehaviour {
+
+	public int FALL_DAMAGE = 60;
+
+	void OnTriggerEnter(Collider col){
+
+		if (col.gameObject.tag.Contains ("Team")) {
+
+			GameObject snowman = col.gameObject;
+			if (snowman.name.Contains("AI"))
+				snowman.GetComponent<AIController>().Health -= FALL_DAMAGE;
+			else
+				snowman.GetComponent<PlayerController>().Health -= FALL_DAMAGE;
+		}
+	}
+}
