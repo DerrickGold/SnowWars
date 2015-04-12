4f2b2087677f1e1577d1b5be82151ca5d563670b
Jay <wickins.jay@gmail.com>
2015-04-12 13:37:00 -0600

    Trap script cleanup

--- Assets/Scripts/Misc/SpikeFall.cs
+++ Assets/Scripts/Misc/SpikeFall.cs
@@ -1,34 +1,41 @@
-﻿using UnityEngine;
+﻿/****************************************************************************************************
+ * Primary Contributor: Jaymeson Wickins
+ * 
+ * Description: This is attached to an empty parent game object of spikes. The game object has a 
+ * collider that will tell this script to drop the spikes whenever a snowman collides with it.  
+ ****************************************************************************************************/
+
+using UnityEngine;
 using System.Collections;
 
 public class SpikeFall : MonoBehaviour {
 
-	public float FALL_SPEED = 1f;
-	private Vector3 originalPosition;
-	private bool fallen;
-	private Transform spikes;
+	public float FALL_SPEED = 1f; //fall speed of the spikes
+	public float FALL_HEIGHT = 65f;
+	private Vector3 originalPosition; //spikes original position
+	private bool fallen; //if the spikes are falling
+	private Transform spikes; //the spikes themselves
 
 	// Use this for initialization
 	void Start () {
-		//originalPosition = transform.position;
-		spikes = transform.Find ("Trap");
-		originalPosition = spikes.position;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+		spikes = transform.Find ("Trap"); //find the child object named "Trap", these are the spikes
+		originalPosition = spikes.position; 
 	}
 
+	//whenever something enters the fall region
 	void OnTriggerEnter(Collider col){
+		//any snowman, AI or player, will have "Team" in their tag
+		//if snowman collides with region, drop spikes
 		if (col.gameObject.tag.Contains ("Team")) StartCoroutine ("fall");
 	}
 
+	//coroutine to drop the spikes over a few seconds
 	public IEnumerator fall(){
-		while (spikes.position.y > 65f) {
+		while (spikes.position.y > FALL_HEIGHT) { //fall a certain distance
 			spikes.position = new Vector3(originalPosition.x, spikes.position.y-FALL_SPEED, originalPosition.z );
 			yield return null;
 		}
-		spikes.position = originalPosition;
+		//after falling a certain distance, revert the spikes back to original position
+		spikes.position = originalPosition; 
 	}
 }
--- Assets/Scripts/Misc/TrapKill.cs
+++ Assets/Scripts/Misc/TrapKill.cs
@@ -1,19 +1,29 @@
-﻿using UnityEngine;
+﻿/****************************************************************************************************
+ * Primary Contributor: Jaymeson Wickins
+ * 
+ * Description: Script that is attached to the spikes themselves to deal damage to any snowman, AI or
+ * player, that the spikes hit. SpikeFall.cs that is attatched to the parent of the spikes is the 
+ * script that controls the spike drop when a snowman is near.
+ ****************************************************************************************************/
+
+using UnityEngine;
 using System.Collections;
 
 public class TrapKill : MonoBehaviour {
 
-	public int FALL_DAMAGE = 60;
+	public int FALL_DAMAGE = 60; //amount of health the spikes take away
 
+	//When the spikes collide with something
 	void OnTriggerEnter(Collider col){
 
+		//Any snowman will have the word "Team" somewhere in the tag
 		if (col.gameObject.tag.Contains ("Team")) {
 
 			GameObject snowman = col.gameObject;
-			if (snowman.name.Contains("AI"))
-				snowman.GetComponent<AIController>().Health -= FALL_DAMAGE;
-			else
-				snowman.GetComponent<PlayerController>().Health -= FALL_DAMAGE;
+			if (snowman.name.Contains("AI")) //if we hit an AI, get the controller script
+				snowman.GetComponent<AIController>().Health -= FALL_DAMAGE; //remove health
+			else //else we hit the player, get the player controller 
+				snowman.GetComponent<PlayerController>().Health -= FALL_DAMAGE; //remove health
 		}
 	}
 }
